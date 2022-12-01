ca21518e3da760ddb986cbaf001c5b9f3886ada9
spexdj <dj_sp3x@hotmail.com>
2022-11-30 23:47:22 -0300

    Correcion en archivo views para post_todos

--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -119,5 +119,5 @@ def buscarpost(request):
 
 def buscar2(request):
     post_views = request.GET['post_nombre']
-    post_todos = Post.objects.filter(post_nombre=post_views)
+    post_todos = Post.objects.filter(post_nombre__contains=post_views)
     return render(request,'AppCoder/resultadoPost.html',{"post_nombre":post_views, "posts": post_todos})
