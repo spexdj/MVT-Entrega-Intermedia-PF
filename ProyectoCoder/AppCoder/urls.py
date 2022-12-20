2683d16c4b4fd867c2c634c8fd2edc8ab1f3e5af
spexdj <dj_sp3x@hotmail.com>
2022-12-20 04:19:53 -0300

    Proyecto Final entrega V1

--- ProyectoCoder/AppCoder/urls.py
+++ ProyectoCoder/AppCoder/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('cursos/', views.cursos,name='Cursos'),
     path('cursosApi/', views.cursosapi),
     path('profesores/', views.profesores),
-    path('busquedaCurso/', views.buscarcurso),
+    path('buscarCurso/', views.buscarcurso,name='Buscar'),
     path('buscar/', views.buscar),
     ############################################
     path('post/', views.post,name='Post'),
@@ -16,6 +16,57 @@ urlpatterns = [
     path('usuarioApi/', views.usuarioapi),
     path('categoria/', views.categoria,name='Categoria'),
     path('categoriaApi/', views.categoriaapi),
-    path('buscar2/', views.buscar2),
-    path('busquedaPost/', views.buscarpost),    
+    #################BUSQUEDAS##################
+    path('buscar2/', views.buscar2,name='BusquedaPost'),
+    path('busquedaPost/', views.buscarpost),
+    path('busquedaCategoria/', views.buscarCategoria),
+    path('buscar3/', views.buscar3,name='BusquedaCategoria'),
+    path('busquedaUsuario/', views.buscarUsuario), 
+    path('buscar4/', views.buscar4,name='BusquedaUsuario'),
+    #################CRUDS##################
+    path('leerCursos/', views.leer_cursos),
+    path('crearcurso/', views.crear_curso),
+    path('editarcurso/', views.editar_curso),
+    path('eliminarcurso/', views.eliminar_curso),
+
+    path('leerposts/', views.leer_posts),
+    path('crearpost/', views.crear_post),
+    path('editarpost/', views.editar_post),
+    path('eliminarpost/', views.eliminar_post),
+
+    path('leercategoria/', views.leer_categoria),
+    path('crearcategoria/', views.crear_categoria),
+    path('editarcategoria/', views.editar_categoria),
+    path('eliminarcategoria/', views.eliminar_categoria),
+
+    path('leerusuarios/', views.leer_usuarios),
+    path('crearusuario/', views.crear_usuario),
+    path('editarusuario/', views.editar_usuario),
+    path('eliminarusuario/', views.eliminar_usuario),    
+
+###############CRUD########################
+    path('curso/list/', views.CursoList.as_view(),name='List'),
+    path('post/list/', views.PostList.as_view(),name='Listpost'),
+    path('categoria/list/', views.CategoriaList.as_view(),name='Listcategoria'),
+    path('usuario/list/', views.UsuarioList.as_view(),name='Listusuario'),
+
+    path('curso/create/', views.CursoCreate.as_view(),name='New'),
+    path('categoria/create/', views.CategoriaCreate.as_view(),name='Newcategoria'),            
+    path('usuario/create/', views.UsuarioCreate.as_view(),name='Newusuario'),            
+    path('post/create/', views.PostCreate.as_view(),name='Newpost'),
+
+    path('curso/edit/<pk>', views.CursoEdit.as_view(),name='Edit'),   
+    path('categoria/edit/<pk>', views.CategoriaEdit.as_view(),name='Editcategoria'),            
+    path('usuario/edit/<pk>', views.UsuarioEdit.as_view(),name='Editusuario'),           
+    path('post/edit/<pk>', views.PostEdit.as_view(),name='Editpost'),
+
+    path('curso/detail/<pk>', views.CursoDetail.as_view(),name='Detail'),   
+    path('categoria/detail/<pk>', views.CategoriaDetail.as_view(),name='Detailcategoria'),            
+    path('usuario/detail/<pk>', views.UsuarioDetail.as_view(),name='Detailusuario'),           
+    path('post/detail/<pk>', views.PostDetail.as_view(),name='Detailpost'),
+
+    path('curso/delete/<pk>', views.CursoDelete.as_view(),name='Delete'), 
+    path('categoria/delete/<pk>', views.CategoriaDelete.as_view(),name='Deletecategoria'),            
+    path('usuario/delete/<pk>', views.UsuarioDelete.as_view(),name='Deleteusuario'),           
+    path('post/delete/<pk>', views.PostDelete.as_view(),name='Deletepost'),    
 ]
--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -40,12 +40,16 @@ def cursosapi(request):
 
 ###vista de busqueda con form###
 def buscarcurso(request):
-    return render(request,"AppCoder/busquedaCurso.html")
+    return render(request,"AppCoder/buscarCurso.html")
 
 def buscar(request):
-    camada_views = request.GET['camada']
-    curso_todos = Curso.objects.filter(camada=camada_views)
-    return render(request,'AppCoder/resultadoCurso.html',{"camada":camada_views, "cursos": curso_todos})
+    if request.get['camada']:
+        camada = request.GET['camada']
+        cursos = Curso.objects.filter(camada=camada)
+        return render(request,'AppCoder/resultadoCurso.html',{"camada":camada, "cursos":cursos})
+    else:
+        respuesta = "No enviaste Datos"
+    return HttpResponse(respuesta)
 
 ###################################
 ###################################
@@ -112,7 +116,7 @@ def categoriaapi(request):
     categoria_todos = Categoria.objects.all()
     return HttpResponse(serializers.serialize('json',categoria_todos))
 
-
+##buscarPosts##
 
 def buscarpost(request):
     return render(request,"AppCoder/busquedaPost.html")
@@ -121,3 +125,197 @@ def buscar2(request):
     post_views = request.GET['post_nombre']
     post_todos = Post.objects.filter(post_nombre__contains=post_views)
     return render(request,'AppCoder/resultadoPost.html',{"post_nombre":post_views, "posts": post_todos})
+
+
+##buscarCategorias##
+
+def buscarCategoria(request):
+    return render(request,"AppCoder/busquedaCategoria.html")
+
+def buscar3(request):
+    categoria_views = request.GET['categoria_nombre']
+    categoria_todos = Categoria.objects.filter(categoria_nombre__contains=categoria_views)
+    return render(request,'AppCoder/resultadoCategoria.html',{"categoria_nombre":categoria_views, "categorias": categoria_todos})
+
+##buscarUsuarios##
+
+def buscarUsuario(request):
+    return render(request,"AppCoder/busquedaUsuario.html")
+
+def buscar4(request):
+    usuario_views = request.GET['usuario_nombre']
+    usuario_todos = Usuario.objects.filter(usuario_nombre__contains=usuario_views)
+    return render(request,'AppCoder/resultadoUsuario.html',{"usuario_nombre":usuario_views, "usuarios": usuario_todos})
+
+##CURSOS##
+
+def leer_cursos(requerst):
+    cursos_all = Curso.objects.all()
+    return HttpResponse(serializers.serialize('json',cursos_all))
+
+def crear_curso(request):
+    curso = Curso(nombre='CursoTest',camada=199,numero_dia=19)
+    curso.save()
+    return HttpResponse(f'Curso {curso.nombre} ha sido creado')
+
+def editar_curso(request):
+    nombre_consulta = 'CursoTest'
+    Curso.objects.filter(nombre=nombre_consulta).update(nombre='NombrenuevoCursoTest')
+    return HttpResponse(f'Curso {nombre_consulta} ha sido actualizado')
+
+
+def eliminar_curso(request):
+    nombre_nuevo='NombrenuevoCursoTest'
+    curso = Curso.objects.get(nombre=nombre_nuevo)
+    curso.delete()
+    return HttpResponse(f'Curso {nombre_nuevo} ha sido eliminado')
+
+
+##POSTS##
+
+def leer_posts(requerst):
+    posts_all = Post.objects.all()
+    return HttpResponse(serializers.serialize('json',posts_all))
+
+def crear_post(request):
+    post = Post(post_nombre='PostTest',post_totalcaracteres=199,post_puntaje=19)
+    post.save()
+    return HttpResponse(f'Post {post.post_nombre} ha sido creado')
+
+def editar_post(request):
+    nombre_consulta_post = 'PostTest'
+    Post.objects.filter(post_nombre=nombre_consulta_post).update(post_nombre='NombrenuevoPostTest')
+    return HttpResponse(f'Post {nombre_consulta_post} ha sido actualizado')
+
+
+def eliminar_post(request):
+    nombre_nuevo_post='NombrenuevoPostTest'
+    post = Post.objects.get(post_nombre=nombre_nuevo_post)
+    post.delete()
+    return HttpResponse(f'Post {nombre_nuevo_post} ha sido eliminado')
+
+##CATEGORIA##
+
+def leer_categoria(requerst):
+    categorias_all = Categoria.objects.all()
+    return HttpResponse(serializers.serialize('json',categorias_all))
+
+def crear_categoria(request):
+    categoria = Categoria(categoria_nombre='CategoriaTest',categoria_importancia=199,categoria_descripcion='Esta es la categoria de CategoriaTest')
+    categoria.save()
+    return HttpResponse(f'Categoria {categoria.categoria_nombre} ha sido creada')
+
+def editar_categoria(request):
+    nombre_consulta_categoria = 'CategoriaTest'
+    Categoria.objects.filter(categoria_nombre=nombre_consulta_categoria).update(categoria_nombre='NombrenuevoCategoriaTest')
+    return HttpResponse(f'Categoria {nombre_consulta_categoria} ha sido actualizada')
+
+
+def eliminar_categoria(request):
+    nombre_nuevo_categoria='NombrenuevoCategoriaTest'
+    categoria = Categoria.objects.get(categoria_nombre=nombre_nuevo_categoria)
+    categoria.delete()
+    return HttpResponse(f'Categoria {nombre_nuevo_categoria} ha sido eliminada')
+
+##USUARIOS##
+
+def leer_usuarios(requerst):
+    usuarios_all = Usuario.objects.all()
+    return HttpResponse(serializers.serialize('json',usuarios_all))
+
+def crear_usuario(request):
+    usuario = Usuario(usuario_nombre='UsuarioTest',usuario_apellido='UsuarioTest2',usuario_mail='maildeusuariotest2@mailfalso.com')
+    usuario.save()
+    return HttpResponse(f'Usuario {usuario.usuario_nombre} ha sido creado')
+
+def editar_usuario(request):
+    nombre_consulta_usuario = 'UsuarioTest'
+    Usuario.objects.filter(usuario_nombre=nombre_consulta_usuario).update(usuario_nombre='NombrenuevoUsuarioTest')
+    return HttpResponse(f'Usuario {nombre_consulta_usuario} ha sido actualizado')
+
+
+def eliminar_usuario(request):
+    nombre_nuevo_usuario='NombrenuevoUsuarioTest'
+    usuario = Usuario.objects.get(usuario_nombre=nombre_nuevo_usuario)
+    usuario.delete()
+    return HttpResponse(f'Usuario {nombre_nuevo_usuario} ha sido eliminado')
+
+
+
+###################################
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.views.generic.detail import DetailView
+
+class CursoList(ListView):
+    model = Curso
+    template = 'AppCoder/curso_list.html'
+class CursoCreate(CreateView):
+    model = Curso
+    fields = '__all__'
+    success_url = '/AppCoder/curso/list'
+class CursoEdit(UpdateView):
+    model = Curso
+    fields = '__all__'
+    success_url = '/AppCoder/curso/list'
+class CursoDetail(DetailView):
+    model = Curso
+    template = 'AppCoder/curso_detail.html'
+class CursoDelete(DeleteView):
+    model = Curso
+    success_url = '/AppCoder/curso/list'
+
+class PostList(ListView):
+    model = Post
+    template = 'AppCoder/post_list.html'
+class PostCreate(CreateView):
+    model = Post
+    fields = '__all__'
+    success_url = '/AppCoder/post/list'
+class PostEdit(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url = '/AppCoder/post/list'
+class PostDetail(DetailView):
+    model = Post
+    template = 'AppCoder/post_detail.html'
+class PostDelete(DeleteView):
+    model = Post
+    success_url = '/AppCoder/post/list'
+
+class CategoriaList(ListView):
+    model = Categoria
+    template = 'AppCoder/categoria_list.html'
+class CategoriaCreate(CreateView):
+    model = Categoria
+    fields = '__all__'
+    success_url = '/AppCoder/categoria/list'
+class CategoriaEdit(UpdateView):
+    model = Categoria
+    fields = '__all__'
+    success_url = '/AppCoder/categoria/list'
+class CategoriaDetail(DetailView):
+    model = Categoria
+    template = 'AppCoder/categoria_detail.html'
+class CategoriaDelete(DeleteView):
+    model = Categoria
+    success_url = '/AppCoder/categoria/list'
+
+
+class UsuarioList(ListView):
+    model = Usuario
+    template = 'AppCoder/usuario_list.html'
+class UsuarioCreate(CreateView):
+    model = Usuario
+    fields = '__all__'
+    success_url = '/AppCoder/usuario/list'
+class UsuarioEdit(UpdateView):
+    model = Usuario
+    fields = '__all__'
+    success_url = '/AppCoder/usuario/list'
+class UsuarioDetail(DetailView):
+    model = Usuario
+    template = 'AppCoder/usuario_detail.html'
+class UsuarioDelete(DeleteView):
+    model = Usuario
+    success_url = '/AppCoder/usuario/list'        
